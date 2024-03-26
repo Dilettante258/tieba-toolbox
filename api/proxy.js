12b38bcd14cd8bd8cf62247b10b4bc5f78becb32
Dilettante258 <2226519330@qq.com>
2024-03-26 23:13:50 +0800

    vercel proxy test

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+import {createProxyMiddleware} from "http-proxy-middleware";
 
-module.exports = (req, res) => {
+
+export default (req, res) => {
   let target = ''
 
   // 代理目标地址
