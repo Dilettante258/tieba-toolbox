6e1ee09df0cb746816ac020a6e5324e031cd4f19
Yuuka <yuuka67@outlook.com>
2025-03-09 23:49:40 +0800

    chores: styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import LandingItem from "@custom/LandingItem";
-import Link from "next/link";
 import Brand from "@custom/Brand";
 import Carousel from "@custom/Carousel";
 import ServiceList from "@custom/ServiceList";
@@ -14,17 +11,6 @@ export default function Home() {
         <Carousel />
       </section>
       <ServiceList />
-      {/*<div className='grid grid-cols-3 justify-between gap-32'>*/}
-      {/*  <LandingItem svg={'/itemIcon1.svg'} title={'用户发言查询'}*/}
-      {/*               desc={'最基础的用户发言查询功能。支持使用ID、UID或用户名进行查询。'}*/}
-      {/*               href={'/userpost'}/>*/}
-      {/*  <LandingItem svg={'/itemIcon1.svg'} title={'关注贴吧查询'}*/}
-      {/*               desc={'查询用户关注的贴吧。对于隐藏的用户，也可进行部分查询。'}*/}
-      {/*               href={'/likeForum'}/>*/}
-      {/*  <LandingItem svg={'/itemIcon1.svg'} title={'用户发言查询'}*/}
-      {/*               desc={'最基础的用户发言查询功能。支持使用ID、UID或用户名进行查询。'}*/}
-      {/*               href={'/userpost'}/>*/}
-      {/*</div>*/}
     </div>
   );
 }
--- components/custom/ServiceList.tsx
+++ components/custom/ServiceList.tsx
@@ -23,7 +23,6 @@ function ServiceList() {
         </div>
       </div>
 
-
     </div>
   )
 }
